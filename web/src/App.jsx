5bfa5c2dc77f0989de7420ca1d0b805e5077828c
AbhiTope <abhijeettope.at@gmail.com>
2025-03-14 09:16:25 +0530

    .

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,14 +1,42 @@
 import { useState } from 'react'
 import './App.css'
+import { Button, DatePicker, Flex, Layout, Splitter } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-	  <h1>Hello World!</h1>
+	  <Profile/>
     </>
   )
+}
+
+function Profile(){
+	
+	return (
+		<>
+		<Layout>
+		<Header  style={ { backgroundColor: "blue", } }>
+
+		<Splitter style={{ height: "100%", boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', }} >
+		<Splitter.Panel defaultSize="70%" min="20%" max="90%">
+		<p>First</p>
+		</Splitter.Panel>
+		<Splitter.Panel>
+		<p>Second</p>
+		</Splitter.Panel>
+		</Splitter>
+
+		</Header>
+		<Content  style={ { backgroundColor: "red", } } >Content</Content>
+		<Footer  style={ { backgroundColor: "green", } } >Footer</Footer>
+		</Layout>
+		</>
+	)
 }
 
 export default App
